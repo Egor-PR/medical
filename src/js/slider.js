2f8d8fb22987604d6bc45bf30e73183f6c864f4e
Egor-PR <beltyugov94@mail.ru>
2022-09-08 19:05:08 +0300

    commit

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,18 +2,18 @@
 let offset = 0; // начальная позиция
 const sliderLine = document.querySelector('.slider__line');
 document.querySelector('.block__button-next').addEventListener('click', function () {
-  offset = offset + 1197;
-  if (ofset > 4580) {
+  offset = offset + 1170;
+  if (offset > 4680) {
     offset = 0;
   }
   sliderLine.style.left = -offset + 'px';
 });
 
-  document.querySelector('.block__button-prev').addEventListener('click', function(){
-    offset = offset - 1197;
-    if (ofset<0) {
-      offset = 4580;
-    }
+document.querySelector('.block__button-prev').addEventListener('click', function () {
+  offset = offset - 1170;
+  if (offset < 0) {
+    offset = 4680;
+  }
 
   sliderLine.style.left = -offset + 'px';
 });
